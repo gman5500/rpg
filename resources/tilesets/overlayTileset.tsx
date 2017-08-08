43c397d7c488b6c3c16438dd0d6eb98f07033924
DKCenaWave <kurathguy@gmail.com>
2017-08-08 04:49:53 -0700

    Added Base and Overlay Tilesets
    Added Test Map

---
+++ resources/tilesets/overlayTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="overlayTileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="overlayTileset.png" trans="f207ff" width="256" height="256"/>
+</tileset>
